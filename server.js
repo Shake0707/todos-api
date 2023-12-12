c50629ad1df5babe727b534b3e94bb594f613e34
Shaxriyor <shaxriyorburiyev7@gmail.com>
2023-12-12 15:57:11 +0300

    setHEaders

--- server.js
+++ server.js
@@ -3,28 +3,32 @@ const router = require('./router.js');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const whitelist = [
-    "https://todos-api-x88y.onrender.com/api/todos",
-    "https://shake0707.github.io/todos/"
-];
-
 const app = express();
 app.use(express.json());
 app.options('*', cors());
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://todos-api-x88y.onrender.com/api"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Alow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+});
 
 const PORT = 8080;
 const DB_URL = "mongodb+srv://shaxriyor:IWDnGduokssZUpUG@todo.fqlpkjf.mongodb.net/?retryWrites=true&w=majority";
 
 const corsOptions = {
+    origin: '*',
     credentials: true,
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200,
+    optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
