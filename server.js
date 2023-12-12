2268756fe9158d23eeb6209974aa948a0e8b2526
Shaxriyor <shaxriyorburiyev7@gmail.com>
2023-12-12 16:54:50 +0300

    change

--- server.js
+++ server.js
@@ -3,24 +3,14 @@ const router = require('./router.js');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const whitelist = [
+    "https://todos-api-x88y.onrender.com/api/todos",
+    "https://shake0707.github.io/todos/"
+];
+
 const app = express();
 app.use(express.json());
 app.options('*', cors());
-
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://todos-api-x88y.onrender.com/api"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Alow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-    );
-});
 
 const PORT = 8080;
 const DB_URL = "mongodb+srv://shaxriyor:IWDnGduokssZUpUG@todo.fqlpkjf.mongodb.net/?retryWrites=true&w=majority";
@@ -39,11 +29,7 @@ const consoleColor = "\x1b[34m";
 
 async function startApp() {
     try {
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            family: 4
-        });
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
         app.listen(PORT, () => console.log(consoleColor, 'Server is started'));
     } catch (e) {
         console.log(e);
